5d33acdd6b421450adc06b845f2079e72547e6b2
Darklove12 <bsinukela31@gmail.com>
2023-02-03 13:50:15 +0200

    added list js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 let balance = document.getElementById('balance')
 let money_plus = document.getElementById('money-plus');
 let money_minus = document.getElementById('money-minus');
+let list =document.getElementById('list');
+let form =document.getElementById('form');
+let text =document.getElementById('text');
