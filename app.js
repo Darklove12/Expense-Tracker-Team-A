bd7bdd82cca41d4932c220b93e4606f1b495d1e2
Darklove12 <bsinukela31@gmail.com>
2023-02-06 09:08:35 +0200

    deleted {} in function

--- app.js
+++ app.js
@@ -1,103 +1,106 @@
 let balance = document.getElementById('balance')
 let money_plus = document.getElementById('money-plus');
 let money_minus = document.getElementById('money-minus');
-let list =document.getElementById('list');
-let form =document.getElementById('form');
-let text =document.getElementById('text');
-let amount =document.getElementById('amount');
+let list = document.getElementById('list');
+let form = document.getElementById('form');
+let text = document.getElementById('text');
+let amount = document.getElementById('amount');
 
 let localStorageTransactions = JSON.parse(
-localStorage.getItem('transations')
+    localStorage.getItem('transations')
 );
 
 let transactions =
     localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
+// Add transaction
 
 
-function addTransation(e){
+
+function addTransation(e) {
     e.preventDefault();
-}
-if (text.value.trim() ==='' || amount.value.trim()===''){
-alert('please add a text and amount');
-}else {
 
-    let transations={
-        id: generateID(),
-        text: text.value,
-        amount: +amount.value
-    };
-    transactions.push(transcations);
+    if (text.value.trim() === '' || amount.value.trim() === '') {
+        alert('please add a text and amount');
+    } else {
 
-    addTransactionDom(transactions);
+        let transations = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        };
+        transactions.push(transcations);
 
-    updateValues();
+        addTransactionDom(transactions);
 
-    updateLocalStorage();
+        updateValues();
 
-    text.value = '';
-    amount.value = '';
+        updateLocalStorage();
 
-}
-{
+        text.value = '';
+        amount.value = '';
+
+    }
+ }
+
     //Generate random ID
-    function generationID(){
+    function generationID() {
         return Math.floor(Math.random() * 100000000);
     }
-    
+
 }
 {
     //Add transactions to DoM list
-    function addTransactionDom(transactions){
-    //Get sign
-    const item = transactions.amount < 0 ? '-' : '+';
+    function addTransactionDom(transactions) {
+        //Get sign
+        const item = transactions.amount < 0 ? '-' : '+';
 
-    const item = document.createElement('li');
+        const item = document.createElement('li');
 
-    // Add class based on value
-    item.classList.add(transactions.amount < 0 ? 'minus' : 'plus');
+        // Add class based on value
+        item.classList.add(transactions.amount < 0 ? 'minus' : 'plus');
 
-    item.innerHTML =`
+        item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs}(
         transaction.amount
     ){</span> <button class="delete-btn" onclick="removeTransaction($"}
     transaction.id
     })">x</button>
     `;
-    list.appendChild(item);
+        list.appendChild(item);
     }
 }
- //Update the balance, income and expense
- function updateValues(){
+//Update the balance, income and expense
+function updateValues() {
     const amount = transactions.map(transaction => transaction.amount);
     const total = amount.reduce((acc, item) => (acc += item, 0).toFixed(2));
     let income = amounts
-    .filter(item => item > 0)
-    .rdeuce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+        .filter(item => item > 0)
+        .rdeuce((acc, item) => (acc += item), 0)
+        .toFixed(2);
 
     let expense = (
-        amounts.filter (item => item < 0).reduce((acc, item) => ( acc += item)) *
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item)) *
         -1
     ).toFixed(2)
 
     balance.innerText = `R${total}`;
-    money_plus.innText= `R${income}`;
+    money_plus.innText = `R${income}`;
 
     money_plus.innerText = `R${expense}`;
- }
+}
 
- // Remove transaction by ID
- function removeTransaction (id){
-    transactions = transactions.filter(transaction => transaction.id ! == id);
+// Remove transaction by ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id! == id);
 
     updateLocalStorage();
 
     init()
- }
-    // Update local storage transa
-    function updateLocalStorage(){
-        
-    }
+}
+// Update local storage transa
+function updateLocalStorage() {
+
+}
 
 
