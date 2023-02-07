1dc6c770a3d6f6aa952a257cbea8bfaf3c7b2ec1
Darklove12 <bsinukela31@gmail.com>
2023-02-07 08:57:12 +0200

    create let list_all

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let balance = document.getElementById('balance');
 let money_high = document.getElementById('money-high');
 let money_less = document.getElementById('money-less');
+let list_all = document.getElementById('list-add');
+
 
